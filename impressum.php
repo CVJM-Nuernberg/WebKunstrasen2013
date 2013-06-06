1ffaaff69a65d873d46f9e41f7842f20f5b9dab6
Stefan <stefan.wagner@logima.de>
2013-06-06 21:17:14 +0200

    init

---
+++ impressum.php
@@ -0,0 +1,19 @@
+<html>
+<head><meta charset="utf-8"></head>
+<body>
+<h1>Impressum</h1>
+
+<p>Verantwortlich für das Kunstrasenfestival 2011 ist der CVJM Nürnberg e.V. in Zusammenarbeit mit verschiedenen Partnern.</p>
+
+<h2>Adresse</h2>
+<p>Kornmarkt 6<br />
+90402 Nürnberg<br />
+Telefon: 0911 20629 0<br />
+Telefax: 0911 20629 33</p>
+
+<p>Kontoverbindung: Acredobank, Nürnberg, Kto.: 350 701 7, BLZ: 760 605 61</p>
+
+<h2>Pflichtangaben</h2>
+<p>Geschäftsführender Vorstand: Dr. Reinhard Prechtel, Dr. Peter Wellmann, Christoph Then, Michael Götz. Registergericht: Amtsgericht - Registergericht - Nürnberg. Registernummer: VR 379. Steuernummer: 241/107/50664 UV4. Inhaltlich Verantwortlicher gemäß § 10 Absatz 3 MDStV: Stefan Wagner (Anschrift wie oben). Mit Namen gekennzeichnete Beiträge geben die Meinung des jeweiligen Autors wieder.</p>
+</body>
+</html>
